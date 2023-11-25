776d899c281c06f4a0cbdfdeb2ef605ee061e89c
Notjcvh <isaacrhong@gmail.com>
2023-11-25 18:44:50 -0500

    Children props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import ListGroup from './components/ListGroup';
-function App(){
-  return <div><ListGroup /></div>
+import Alert from "./components/Alert";
+function App() {
+  return (
+    <div>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+    </div>
+  );
 }
 
 export default App;
